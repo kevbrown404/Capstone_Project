ab38921e7531b22e7e274fad1299ace16f4a1f10
Kevin Brown <kevinbrown@Kevins-Air.lan>
2023-07-17 18:29:30 -0700

    deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#Install dependencies
+pip3 install -r deps.txt
 
-pip3 install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+#Run migrations
+python3 manage.py migrate
